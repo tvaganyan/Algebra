7885b4548b5e00d7274f11823828a4a379b9adea
Tigran Vaganyan <tvaganyan@yandex.ru>
2026-01-17 21:47:31 +0300

    NormalDistribution

--- lib/src/main/java/sequences/NormalDistribution.java
+++ lib/src/main/java/sequences/NormalDistribution.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class NormalDistribution {
     private int dim;
+    private int size;
     private List<Vector> sequence;
     private Vector average;
     private Matrix dispersion;
@@ -20,12 +21,13 @@ public class NormalDistribution {
     public NormalDistribution(List<Vector> sequence){
         dim = sequence.get(0).getDim();
         fc = sequence.get(0).getFc();
+        size = sequence.size();
         this.sequence = sequence;
         average = new Vector(dim, fc);
         average.o();
         noise = new Matrix(dim, fc);
         noise.o();
-        for(int k = 1; k < sequence.size(); k++) {
+        for(int k = 1; k < size; k++) {
             average.sum(sequence.get(k), average);
             for (int i = 0; i < dim; i++) {
                 for (int j = 0; j < dim; j++) {
@@ -39,26 +41,26 @@ public class NormalDistribution {
                 }
             }
         }
-        Field n = fc.getN(sequence.size());
-        n.div(fc.get1(), n);
-        average.scalarMul(n, average);
-        noise.scalarMul(n, noise);
+        Field s = fc.getN(size - 1);
+        s.div(fc.get1(), s);   //    s = 1 / (size - 1)
+        average.scalarMul(s, average);
+        noise.scalarMul(s, noise);
         dispersion = new Matrix(dim, fc);
         dispersion.o();
-        for(Vector v: sequence) {
+        for(int k = 1; k < size; k++) {
             for (int i = 0; i < dim; i++) {
                 for (int j = 0; j < dim; j++) {
                     Field x = fc.get0();
                     Field y = fc.get0();
                     Field z = fc.get0();
-                    x.dif(v.getV()[i], average.getV()[i]);
-                    y.dif(v.getV()[j], average.getV()[j]);
+                    x.dif(sequence.get(k).getV()[i], average.getV()[i]);
+                    y.dif(sequence.get(k).getV()[j], average.getV()[j]);
                     z.mul(x, fc.conjugate(y));
                     dispersion.getM()[i][j].sum(z, dispersion.getM()[i][j]);
                 }
             }
         }
-        dispersion.scalarMul(n, dispersion);
+        dispersion.scalarMul(s, dispersion);
         dispersionInv = new Matrix(dim, fc);
         dispersionInv.inverse(dispersion);
     }
